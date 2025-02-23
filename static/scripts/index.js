158e29a462894fa77df908f66afdca3469c42ad5
branila <stefan.branila55@gmail.com>
2025-02-23 22:28:25 +0100

    refactoring

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -23,6 +23,33 @@ theButton.addEventListener('click', event => {
   }, 500)
 })
 
+const username = localStorage.getItem('username')
+
+if (username) {
+  document.querySelector('.content').classList.remove('hidden')
+  connectWebSocket()
+} else {
+  document.querySelector('.registration').classList.remove('hidden')
+}
+
+const registerButton = document.querySelector('.registration button')
+
+registerButton.addEventListener('click', () => {
+  const username = document.querySelector('.registration input').value
+
+  if (!username) {
+    alert('Scrivi un nome sciocchino/a')
+    return
+  }
+
+  localStorage.setItem('username', username.toLowerCase())
+
+  document.querySelector('.content').classList.remove('hidden')
+  document.querySelector('.registration').classList.add('hidden')
+
+  connectWebSocket()
+})
+
 function connectWebSocket() {
     ws = new WebSocket(`ws://${window.location.host}/ws`)
 
